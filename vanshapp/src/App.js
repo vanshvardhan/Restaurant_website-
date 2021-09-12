88e6eafa4133ce0eceaaaa6da7b34271b545c301
Vansh <vansh.vardhan55607@gmail.com>
2021-09-11 22:08:18 -0400

    Initial commit

---
+++ vanshapp/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Restaurant from "./component/Basics/Restaurant"
+
+const App = () => {
+  return (
+    <Restaurant/>
+  )
+}
+
+export default App
---
+++ vanshapp/src/component/Basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/poha.jpg",
+      name: "poha",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        " Poha is rice that has been parboiled, rolled, flattened and then dried to produce flakes. The flakes come in different thicknesses depending on the pressure used in the flattening process. Rice flakes are small, very light, about 2mm long, flat and greyish white in colour. ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "Baby corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Baby corn (also known as young corn, cornlets or baby sweetcorn) is a cereal grain taken from corn (maize) harvested early while the stalks are still small and immature. It typically is eaten whole – cob included – in contrast to mature corn, whose cob is too tough for human consumption ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola Batura",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Chole bhature is a food dish popular in the Northern areas of the Indian subcontinent. It is a combination of chana masala (spicy white chickpeas) and bhatura/puri, a fried bread made from maida. ",
+    },
+    {
+      id: 5,
+      image: "../images/vadapav.jpg",
+      name: "vada pav",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Vada pav, alternatively spelt wada pao, is a vegetarian fast food dish native to the state of Maharashtra. The dish consists of a deep fried potato dumpling placed inside a bread bun sliced almost in half through the middle. ",
+    },
+    {
+      id: 6,
+      image: "../images/vegthali.jpg",
+      name: "Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Thali is a round platter used to serve food in South Asia and Southeast Asia. Thali is also used to refer to an Indian-style meal made up of a selection of various dishes which are served on a platter ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Dessert (/dɪˈzɜːrt/) is a course that concludes a meal. The course consists of sweet foods, such as confections, and possibly a beverage such as dessert wine and liqueur. ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma Chawal is perfect for a Sunday brunch or on a get-together. It is cooked by using easily available ingredients like rajma (kidney beans) rice, onions, tomatoes and a selection of spices.  ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "A samosa is a South Asian fried or baked pastry with a savory filling like spiced potatoes, onions, peas, chicken and other meats, or lentils",
+    },
+  ];
+  
+  export default Menu;
