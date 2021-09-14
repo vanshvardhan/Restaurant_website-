f69ce22a84dd6d72710e0b5f4ca4cbf543f506c0
Vansh <vansh.vardhan55607@gmail.com>
2021-09-14 08:07:39 -0400

    first commit

--- vanshapp/src/component/Basics/menuApi.js
+++ vanshapp/src/component/Basics/menuApi.js
@@ -12,7 +12,7 @@ const Menu = [
     {
       id: 2,
       image: "images/allupakoida.jpg",
-      name: "allu pakoda",
+      name: "aaloo pakoda",
       category: "evening",
       price: "20₹",
       description:
